676a178b37b41c2e8324345dce9ad5155cfee1a8
Dmitrii <stepanovdimamain@gmail.com>
2025-05-24 22:02:25 +0400

    Backend setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,18 @@
 import 'module-alias/register';
+
+import { connectDB } from '@lib/db';
+import dotenv from 'dotenv';
 import express from 'express';
+
 import authRoutes from './routes/auth.route';
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
+dotenv.config();
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server started on http://localhost:${PORT}`);
+  connectDB();
 });
